8b8eac40dc5021cf14c0a693dff4e1d223c62f55
Corey Petty <corey@status.im>
2024-04-12 11:48:09 -0400

    the rest

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "NixOS configuration for my personal hosts.";
+
+  inputs = {
+    # nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    unstable.url = "nixpkgs/nixos-unstable";
+    hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, unstable, hardware }:
+    let
+      overlay = final: prev: {
+        unstable = import unstable { inherit (prev) system; config.allowUnfree = true; };
+      };
+      # Overlays-module makes "pkgs.unstable" available in configuration.nix
+      overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
+      # To generate host configurations for all hosts.
+      hostnames = builtins.attrNames (builtins.readDir ./hosts);
+      # Some hosts are ARM64
+      systemForHost = hostname:
+        if builtins.elem hostname ["host1" "host2"] then "aarch64-linux"
+        else "x86_64-linux";
+    in {
+      nixosConfigurations = builtins.listToAttrs (builtins.map (host: {
+        name = host;
+        value = nixpkgs.lib.nixosSystem {
+          system = systemForHost host;
+          specialArgs.channels = { inherit nixpkgs unstable hardware; };
+          modules = [ overlayModule ./hosts/${host}/configuration.nix ];
+        };
+      }) hostnames);
+    };
+}
---
+++ hosts/rooter/configuration.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../roles/hyprland.nix
+    ../../roles/info-fetchers.nix
+    ../../roles/gnome.nix
+    ../../roles/display-manager.nix
+    ../../roles/screen.nix
+    ../../roles/theme.nix
+    ../../roles/users.nix
+    ../../roles/nix-settings.nix
+    ../../roles/services.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.grub.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
+
+  # Resume
+  systemd.sleep.extraConfig = "HibernateMode=reboot";
+
+  # Sensors
+  boot.kernelModules = [ "nct6775" "coretemp" "i2c-1" ];
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
+
+  networking = {
+    hostName = "rooter";
+    domain = "enderverse";
+    # useDHCP = true;
+    networkmanager.enable = true;
+  };
+
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+  # Avoid difference when dual-booting Windows.
+  time.hardwareClockInLocalTime = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  
+
+  # Video
+  services.xserver.videoDrivers = [ "amdgpu-pro" ];
+
+  # Power Management
+  powerManagement.cpuFreqGovernor = "performance";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  # system.stateVersion = "20.09";
+
+  systemd.coredump.enable = true;
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-24.8.6"
+    "electron-25.9.0"
+  ];
+
+  # allow video into loopback for OBS
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+
+  # enable virtualization
+  virtualisation.libvirtd.enable = true;
+}
