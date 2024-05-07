33020ef99376b09d563b1360d082c8097db5f185
Corey Petty <corey@status.im>
2024-05-06 20:11:57 -0400

    added firewall

--- roles/security.nix
+++ roles/security.nix
@@ -1,14 +1,38 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
 {
-  security.pam.services.swaylock = {};
+  # Drop packets by default.
+  networking.firewall = {
+    enable = true;
+    extraCommands = ''
+      iptables -A INPUT -p tcp --dport 7860 -s 192.168.0.0/24 -j ACCEPT
+      iptables -A INPUT -p tcp --dport 7860 -s 127.0.0.0/8 -j ACCEPT
+      iptables -A INPUT -p tcp --dport 8188 -s 127.0.0.0/8 -j ACCEPT
+      iptables -A INPUT -p tcp --dport 8188 -s 192.168.0.0/24 -j ACCEPT
+      iptables -P INPUT DROP
+      iptables -P FORWARD DROP
+    '';
+    allowedTCPPorts = [ 7860 8188 22 ];
+  };
 
-  environment.systemPackages = with pkgs; [
-    pass-wayland
-    passExtensions.pass-otp
-    passExtensions.pass-tomb
-    passExtensions.pass-update
-    passExtensions.pass-import
-    tomb
-  ];
+  # Increase SSH security.
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # Enable GnuPG agent for keys.
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    enableBrowserSocket = true;
+    pinentryFlavor = "gnome3";
+  };
+
+  # Use PAM with SSH auth.
+  security.pam.enableSSHAgentAuth = true;
 }
