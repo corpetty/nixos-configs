1f96cf1428eba90e210d9c299ca74c67464f4e7c
Corey Petty <corey@status.im>
2024-05-17 11:05:19 -0400

    updated hyprland flake input

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     hardware.url = "github:NixOS/nixos-hardware/master";
 
     hyprland = {
-      url = "github:hyprwm/hyprland";
+      url = ""git+https://github.com/hyprwm/Hyprland?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
