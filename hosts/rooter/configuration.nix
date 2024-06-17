9226e1f647182dae4338d09828d8e7cc3f485e49
Corey Petty <corey@status.im>
2024-06-17 15:42:42 -0400

    rooter config change

--- hosts/rooter/configuration.nix
+++ hosts/rooter/configuration.nix
@@ -12,6 +12,7 @@
       # ../../roles/display-manager.nix
       ../../roles/hyprland.nix
       ../../roles/services.nix
+      ../../roles/theme.nix
     ];
 
   # NixOS recommends using this on AMD platforms
